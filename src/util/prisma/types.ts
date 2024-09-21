eebf943e8aad036681da12c3b798523c86e7aadc
GSDV <sdvgab@gmail.com>
2024-09-21 12:54:40 -0500

    Types

--- src/util/prisma/types.ts
+++ src/util/prisma/types.ts
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client';
 
-// export type UserWithPosts = Prisma.UserGetPayload<{ include: {posts: true} }>;
+export type User = Prisma.UserGetPayload<{}>;
 export type RedactedUser = Prisma.UserGetPayload<{ omit: {password: true, salt: true} }>;
 export type RedactedUserWithPosts = Prisma.UserGetPayload<{ omit: {password: true, salt: true}, include: {posts: true} }>;
 export type PostWithRedactedUser = Prisma.PostGetPayload<{ include: {seller: {omit: {password: true, salt: true}}} }>;
